12ad82e0ce8fd6a6b0c5bfab36d06580f629e86b
neizvesno-kto <guram_zhorzholiani@mail.ru>
2024-01-12 19:14:32 +0300

    Создали схему

--- capybaras/mongooseTasks.js
+++ capybaras/mongooseTasks.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/test');
 
-const Capybar = mongoose.model('Capybar', { name: String });
-const welding = new Capybar({ name: 'Апельсин' });
+const CapybaraSchema = new mongoose.Schema( {name: String} );
+CapybaraSchema.methods.brew = function() {
+    console.log(this.name + ' ест апельсины');
+}
+const Capybar = mongoose.model('Capybara', CapybaraSchema);    
+const welding = new Capybar({ name: 'Капибара с вилкой' });
 
 welding.save()
-  .then(() => console.log('на_голове'))
+  .then(() => welding.brew('на голове'))
   .catch((err) => console.log(err));
