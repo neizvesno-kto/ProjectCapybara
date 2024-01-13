ae92312b3ef74fdac73d8235c5a36cf585d1f511
neizvesno-kto <guram_zhorzholiani@mail.ru>
2024-01-13 19:53:32 +0300

    Заполнили меню из базы данных

--- capybaras/routes/capybara.js
+++ capybaras/routes/capybara.js
@@ -1,27 +1,36 @@
 var express = require('express');
 var router = express.Router();
 var Capybara = require("../models/capybara").Capybara
+const async = require("async")
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Новый маршрутизатор, для маршрутов, начинающихся с Capybaras');
+router.get('/:nick', async function(req, res, next) {
+  try {
+    const [capybara, capybaras] = await Promise.all([
+      Capybara.findOne({ nick: req.params.nick }),
+      Capybara.find({}, { _id: 0, title: 1, nick: 1 })
+    ]);
+  
+    if (!capybara) {
+      throw new Error("Нет такой капибары");
+    }
+    
+    renderTree(res, capybara.title, capybara.avatar, capybara.desc, capybaras);
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.get("/:nick", async (req, res, next) => {
-    try {
-      const capybara = await Capybara.findOne({ nick: req.params.nick });
-      console.log(capybara);
-      if (!capybara) {
-        throw new Error("Нет такой капибары");
-      }
-      res.render('capybara', {
-        title: capybara.title,
-        picture: capybara.avatar,
-        desc: capybara.desc
-      });
-    } catch (err) {
-      next(err);
-    }
+function renderTree(res, title, picture, desc, capybaras) {
+  console.log(capybaras);
+
+  res.render('capybara', {
+    title: title,
+    picture: picture,
+    desc: desc,
+    menu: capybaras
   });
-       
+}
+
+
+
 module.exports = router;
