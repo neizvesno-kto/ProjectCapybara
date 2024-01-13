226973d530c8babea0417224c90728e136a51a8e
neizvesno-kto <guram_zhorzholiani@mail.ru>
2024-01-13 20:42:26 +0300

    Изучили команду записи

--- capybaras/routes/index.js
+++ capybaras/routes/index.js
@@ -6,6 +6,11 @@ const Capybara = require("../models/capybara").Capybara;
 router.get('/', async (req, res, next) => {
   try {
     const menu = await Capybara.find({}, { _id: 0, title: 1, nick: 1 });
+    res.cookie('greeting', 'Hi!!!').render('index', { 
+      title:'Express', 
+      menu:menu 
+    });
+    req.session.greeting = "Hi!!!"
     res.render('index', {
       title: 'Express',
       menu: menu
