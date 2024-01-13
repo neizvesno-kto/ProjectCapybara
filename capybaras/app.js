7e91b1218176774e5a2800557a409435faa4af47
neizvesno-kto <guram_zhorzholiani@mail.ru>
2024-01-13 17:09:43 +0300

    изучили обработку параметров в адресе

--- capybaras/app.js
+++ capybaras/app.js
@@ -8,6 +8,7 @@ mongoose.connect('mongodb://localhost/capybaras')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var capybaras = require('./routes/capybaras');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/capybaras', capybaras);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
