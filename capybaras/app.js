1cd7856fdf41548c8692c686814171a41059f28e
neizvesno-kto <guram_zhorzholiani@mail.ru>
2024-01-13 20:14:01 +0300

    Установили модуль EXPRESS-SESSION и настроили cookie

--- capybaras/app.js
+++ capybaras/app.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/capybaras')
+var session = require("express-session")
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -22,6 +24,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: "ThreeCapybaras",
+  cookie:{maxAge:60*1000},
+  resave: true,
+  saveUninitialized: true
+  }))
+  
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
