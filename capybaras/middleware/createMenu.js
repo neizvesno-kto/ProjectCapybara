7590bb18af4bdab5e38a83825f9d97399bf92a33
neizvesno-kto <guram_zhorzholiani@mail.ru>
2024-01-15 00:11:45 +0300

    Заполнили меню из базы

--- capybaras/middleware/createMenu.js
+++ capybaras/middleware/createMenu.js
@@ -1,12 +1,10 @@
-const Capybara = require("./../models/capybara").Capybara;
+const db = require('./../mySQLConnect');
 
-module.exports = async function(req, res, next) {
-  try {
-    res.locals.nav = [];
-    const result = await Capybara.find({}, { _id: 0, title: 1, nick: 1 });
-    res.locals.nav = result;
-    next();
-  } catch (err) {
-    throw err;
-  }
+module.exports = function(req, res, next) {
+  res.locals.nav = [];
+  db.query('SELECT title, nick FROM capybaras', function(err, result) {
+      if (err) throw err;
+      res.locals.nav = result;
+      next();
+  });
 };
