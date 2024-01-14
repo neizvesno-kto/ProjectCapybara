37d7b2899dad3e0091fd57ffdc6cee4f519d6daa
neizvesno-kto <guram_zhorzholiani@mail.ru>
2024-01-15 01:10:22 +0300

    ВОССТАНАВЛИВАЕМ ЗАКРЫТИЕ СТРАНИЦ ДЛЯ НЕЗАРЕГИСТИРОВАННОГО ПОЛЬЗОВАТЕЛЯ

--- capybaras/routes/capybara.js
+++ capybaras/routes/capybara.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../mySQLConnect.js');
+const async = require("async")
+var checkAuth = require("./../middleware/checkAuth.js")
+
 router.get('/', function(req, res, next) {
 res.send('<h1>Это экран для списка капибар</h1>');
 });
-router.get("/:nick", function(req, res, next) {
+router.get("/:nick", checkAuth, function(req, res, next) {
 db.query(`SELECT * FROM capybaras WHERE capybaras.nick = '${req.params.nick}'`, (err, capybaras) => {
 if(err) {
 console.log(err);
