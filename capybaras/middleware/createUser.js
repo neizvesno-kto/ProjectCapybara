55ce8eb7aa62913ac91d24ceaacb04e0379d45d5
neizvesno-kto <guram_zhorzholiani@mail.ru>
2024-01-15 01:05:21 +0300

    ИНФОРМАЦИЯ О ПОЛЬЗОВАТЕЛЕ

--- capybaras/middleware/createUser.js
+++ capybaras/middleware/createUser.js
@@ -1,15 +1,22 @@
-const User = require("../models/User").User;
-
-module.exports = async function(req, res, next) {
-     res.locals.user = null;
-//   try {
-//     const user = await User.findById(req.session.user);
-  
-//     res.locals.user = user;
-//     next();
-//   } catch (err) {
-//     next(err);
-//   }
-    next();
-    
-};
+//var User = require("./../models/User").User
+var db = require('../mySQLConnect.js');
+module.exports = function(req,res,next) {
+res.locals.user = null
+console.log('Проверяем сессию')
+console.log(req.session.user)
+db.query(`SELECT * FROM user WHERE user.id = '${req.session.user}'`, (err,
+users) => {
+if(err)
+return next(err)
+if(users.length > 0) {
+res.locals.user = users[0];
+}
+next()
+})
+// User.findById(req.session.user, function (err, user) {
+// if (err)
+// return next(err)
+// res.locals.user = user;
+// next();
+// })
+}
