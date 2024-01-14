1360e69bb69104ca0887d41ba2fb4902f0eb129e
neizvesno-kto <guram_zhorzholiani@mail.ru>
2024-01-15 01:01:16 +0300

    СОЗДАНИЕ ТАБЛИЦЫ USER, ЛОГИКА ПОЛЬЗОВАТЕЛЯ

--- capybaras/routes/index.js
+++ capybaras/routes/index.js
@@ -1,49 +1,55 @@
 const express = require('express');
 const router = express.Router();
-const Capybara = require("../models/capybara").Capybara;
-var User = require("./../models/User").User
+//var User = require("./../models/User").User
+var db = require('../mySQLConnect.js');
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
   try {
-
     req.session.greeting = "Hi!!!"
     res.render('index', { title: 'Express', counter:req.session.counter });
   } catch (err) {
     next(err);
   }
 });
+
 router.get('/logreg', async function(req, res, next) {
   res.render('logreg', { title: 'Вход',error:null}); 
 });
-  router.post('/logreg', async function(req, res, next) {
-    const username = req.body.username;
-    const password = req.body.password;
-    try {
-        const user = await User.findOne({ username });
-        
-        if (user) {
-            if (user.checkPassword(password)) {
-                req.session.user = user._id;
-                res.redirect('/');
-            } else {
-                res.render('logreg', { title: 'Вход', error: 'Неправильный пароль' });
-            }
-        } else {
-            const newUser = new User({ username, password });
-            await newUser.save();
-            req.session.user = newUser._id;
-            res.redirect('/');
-        }
-    } catch (err) {
-        next(err);
-    }
-  });
 
-  router.post('/logout', (req, res, next) => {
-    req.session.destroy();
-    res.locals.user = null;
-    res.redirect('/');
+router.post('/logreg', function(req, res, next) {
+  var username = req.body.username
+  var password = req.body.password
+  db.query (`SELECT * FROM user WHERE user.username = '${req.body.username}'`,
+  function(err,users){
+  if(err) return next(err)
+  if(users.length > 0) {
+  var user = users[0];
+  if (password == user.password){
+  req.session.user = user.id
+  res.redirect('/')
+  } else {
+    res.render('logreg', { title: 'Вход',error:"Неверный пароль"}); 
+  }
+  } else {
+  db.query(`INSERT INTO user (username, password) VALUES ('${username}',
+  '${password}')`, function(err, user){
+  if(err) return next(err)
+  req.session.user = user.id
+  res.redirect('/')
+  })
+  }
+  })
   });
+  
+router.post('/logout', function(req, res, next) {
+  req.session.destroy()
+  res.locals.user = null
+  res.redirect('/')
+});
+  
+
+
+
 
 module.exports = router;
