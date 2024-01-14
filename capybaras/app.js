9a7cd80702c385c6295e75f1edec2058b4e4b67b
neizvesno-kto <guram_zhorzholiani@mail.ru>
2024-01-14 21:15:51 +0300

    Сделали переменную User глобальной

--- capybaras/app.js
+++ capybaras/app.js
@@ -39,6 +39,8 @@ app.use(session({
     })
 
 app.use(require("./middleware/createMenu.js"));
+app.use(require("./middleware/createUser.js"))
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/capybaras', capybaras);
---
+++ capybaras/middleware/createUser.js
@@ -0,0 +1,10 @@
+var User = require("./../models/User").User
+module.exports = function(req,res,next) {
+res.locals.user = null
+User.findById(req.session.user, function (err, user) {
+if (err)
+return next(err)
+res.locals.user = user;
+next();
+})
+}
